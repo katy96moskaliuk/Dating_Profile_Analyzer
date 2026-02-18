6955737524b7fc56c2676252a353d33040c9244e
Kate Moskaliuk <katy96ms@gmail.com>
2026-02-18 19:42:56 +0200

    Created class Profile

--- my_project.py
+++ my_project.py
@@ -1,1 +1,10 @@
+class Profile:
+	def __init__(self, text: str):
+		self.text = text.strip()
 
+	def __repr__(self):
+		return f"Profile (text_lenght = ({len(self.text)})"
+	
+p = Profile(" Kate 25 fine")
+print(p.text)
+print(p)
