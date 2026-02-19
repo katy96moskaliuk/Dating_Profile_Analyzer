3edfeb028f4b9f4dddebbf3c0c0e1435e73911a5
Kate Moskaliuk <katy96ms@gmail.com>
2026-02-19 16:26:48 +0200

    Add class StyleAnalizer

--- my_project.py
+++ my_project.py
@@ -13,3 +13,21 @@ class AnalysisResult:
 	
 	def __repr__(self):
 		return f"AnalysisResult(style={self.style}, red_flags={self.red_flags}, score={self.score})"
+	
+class StyleAnalizer:
+
+	ROMANTIC_WORDS = {"love", "soul", "deep", "connection"} 
+	PARTY_WORDS = {"fun", "club", "party", "drink"} 
+	CAREER_WORDS = {"ambitious", "career", "success", "goal"}
+
+	def analyze(self, text: str) -> str:
+
+		words = set(text.lover.split())
+		if words & self.ROMANTIC_WORDS:
+			return "Romantic"
+		elif words & self.PARTY_WORDS:
+			return "Party Lover"
+		elif words & self.CAREER_WORDS:
+			return "Career-Oriented"
+		else:
+			return "Neutral"
